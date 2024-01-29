1029cbe4895a8935c20ec4b3f18ff8cc3d834e8c
Gabriella Marusyak <slipuhinag@gmail.com>
2024-01-29 16:35:22 +0200

    Changed mongoDb URL

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const PORT: string | number = process.env.PORT || 4000
-const DB_HOST: string = process.env.DB_HOST || "";
+// const PORT: string | number = process.env.PORT || 4000
+// const DB_HOST: string = process.env.DB_HOST || "";
 
-mongoose.connect(DB_HOST)
+
+mongoose.connect('mongodb+srv://Gabriella:MkrzUBn7szgS0Tt6@cluster0.i5vmm9f.mongodb.net/db-rolio?retryWrites=true&w=majority'
+)
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(8000, () => {
       console.log("Database connection successful")
-      console.log(`Server running. Use our API on port: ${PORT} `)
+      console.log(`Server running. Use our API on port: ${8000} `)
 
     })
   })
