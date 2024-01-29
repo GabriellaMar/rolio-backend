7e358a5cf27403c3736088e9dfa80bc1825e5e3d
Gabriella Marusyak <slipuhinag@gmail.com>
2024-01-29 17:04:45 +0200

    Added some response

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ dotenv.config();
 
 // const PORT: string | number = process.env.PORT || 4000
 // const DB_HOST: string = process.env.DB_HOST || "";
-
+app.use((req,res)=>{
+    res.send('Hello')
+})
 
 mongoose.connect('mongodb+srv://Gabriella:MkrzUBn7szgS0Tt6@cluster0.i5vmm9f.mongodb.net/db-rolio?retryWrites=true&w=majority'
 )
@@ -14,6 +16,7 @@ mongoose.connect('mongodb+srv://Gabriella:MkrzUBn7szgS0Tt6@cluster0.i5vmm9f.mong
     app.listen(8000, () => {
       console.log("Database connection successful")
       console.log(`Server running. Use our API on port: ${8000} `)
+      console.log('Server listening http://localhost:8000')
 
     })
   })
