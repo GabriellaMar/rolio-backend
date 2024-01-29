5b00fff02a9c7796ca8f3c54352d4245d403cb82
Gabriella Marusyak <slipuhinag@gmail.com>
2024-01-29 14:41:39 +0200

    Created successful Database connection

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const PORT: string | number = process.env.PORT || 4000
+const DB_HOST: string = process.env.DB_HOST || "";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful")
+      console.log(`Server running. Use our API on port: ${PORT} `)
+
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
